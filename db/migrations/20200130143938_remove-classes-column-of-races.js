c2725047cf2ed1ac93d1c081ba5e21894c333dfe
Raisa Primerova <riyuhide@gmail.com>
2020-01-30 14:52:31 -0700

    Remove classes column from races table

---
+++ db/migrations/20200130143938_remove-classes-column-of-races.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.table('races', function(table) {
+        table.dropColumn('classes');
+    });
+};
+
+exports.down = function(knex) {
+  knex.schema.table('users', function(table) {
+    table.string('classes');
+  });
+};

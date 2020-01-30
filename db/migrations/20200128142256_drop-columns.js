e912b2a57755b5321ace20e3aa09155e22b5af1b
Raisa Primerova <riyuhide@gmail.com>
2020-01-30 09:56:15 -0700

    Create schema for 'races' table

---
+++ db/migrations/20200128142256_drop-columns.js
@@ -0,0 +1,23 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('races', function (table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('faction');
+      table.string('race_symbol');
+      table.string('race_image');
+      table.text('description');
+      table.text('history');
+      table.string('starting_zone');
+      table.string('home_city');
+      table.string('leader');
+      table.string('mount');
+      table.string('classes');
+      table.timestamps(true, true);
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('races')
+};

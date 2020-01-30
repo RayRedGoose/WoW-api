6297dc7da8047ed5ba420dde87f068568c59a97d
Raisa Primerova <riyuhide@gmail.com>
2020-01-30 11:07:57 -0700

    Update database and packages

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/wow_project',
+    connection: 'postgres://localhost/wow_api',
     migrations: {
       directory: './db/migrations'
     },

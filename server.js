f2be72684fafe7da438e133bccc95fb16b256e59
Raisa Primerova <riyuhide@gmail.com>
2020-01-30 22:36:40 -0700

    Change var to const

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-  var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];

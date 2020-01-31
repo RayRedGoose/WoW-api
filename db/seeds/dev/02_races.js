10987e4ef8c9bf69b014a80ddcc22a62a7f097f6
Raisa Primerova <riyuhide@gmail.com>
2020-01-30 20:19:45 -0700

    Seed race-class pairs to joining table

--- db/seeds/dev/02_races.js
+++ db/seeds/dev/02_races.js
@@ -1,22 +1,30 @@
 const racesData = require('../../../data/races');
 
-const getAllClasses = async (knex, race) => {
-  const classes = await race.classes.map(async (classEl) => {
+const getAllClasses = async (knex, classes, raceID) => {
+  const classesPromises = await classes.map(async (classEl) => {
     const el = await knex('classes').where({name: classEl}).returning('id');
-    return el[0]['id']
-  });
 
-  return Promise.all(classes);
+    return knex('raceClasses').insert({
+      race_id: raceID,
+      class_id: el[0].id
+    });
+  });
+  return Promise.all(classesPromises);
 }
 
 exports.seed = async (knex) => {
   try {
     await knex('races').del();
+    await knex('raceClasses').del();
 
     let racesPromises = racesData.map(async (race) => {
-      const allClasses = await getAllClasses(knex, race);
-      return knex('races').insert({...race, classes: JSON.stringify(allClasses)});
+      const { name, race_image, race_symbol, faction, description, history, starting_zone, home_city, leader, mount } = race;
+
+      const raceID = await knex('races').insert({ name, race_image, race_image, faction, description, history, starting_zone, home_city, leader, mount }, 'id');
+
+      getAllClasses(knex, race.classes, raceID[0]);
     });
+
     return Promise.all(racesPromises);
   } catch (error) {
     console.log(`Error seeding data: ${error}`)
